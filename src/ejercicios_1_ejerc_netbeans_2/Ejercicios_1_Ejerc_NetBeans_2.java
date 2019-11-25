73d30bf7447b8571febb4aab8d659c61d89b32b5
usuario <usuario@usuario-PC>
2019-11-25 09:31:02 +0100

    2019/11/18 09:18

---
+++ src/ejercicios_1_ejerc_netbeans_2/Ejercicios_1_Ejerc_NetBeans_2.java
@@ -0,0 +1,12 @@
+package ejercicios_1_ejerc_netbeans_2;
+/** @author usuario */
+public class Ejercicios_1_Ejerc_NetBeans_2 {
+
+    public static void main(String[] args) {
+        
+    for(int num=1; num<=1000; num++)  
+       	if (num % 2 ==0)
+        System.out.println ("El número "+num+" es par" );
+    }
+    
+}
